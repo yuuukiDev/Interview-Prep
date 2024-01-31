97da224441daa0c66e6d48f6085000be4b12036a
yuuukiDev <mohamedemadeldin34@gmail.com>
2024-01-31 17:24:37 +0200

    Day 0

--- HackerRank-Mastery/Badges/30 Days Of Code/Hello, World.js
+++ HackerRank-Mastery/Badges/30 Days Of Code/Hello, World.js
@@ -3,5 +3,5 @@ function processData(inputString) {
     console.log("Hello, World.");
     
     // Write the second line of output that prints the contents of 'inputString' here.
-    console.log("Welcome to 30 Days of Code!")    
+    console.log(inputString);    
 }
